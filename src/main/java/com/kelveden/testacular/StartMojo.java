112144347804afac506f21868a610d23f0416174
Alistair Dutton <alistair.dutton@nokia.com>
2013-03-13 00:15:22 +0000

    Reverted back to com.kelveden as group id.

--- src/main/java/com/kelveden/testacular/StartMojo.java
+++ src/main/java/com/kelveden/testacular/StartMojo.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.kelveden.plugins.testacular;
+package com.kelveden.testacular;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.maven.plugin.AbstractMojo;
