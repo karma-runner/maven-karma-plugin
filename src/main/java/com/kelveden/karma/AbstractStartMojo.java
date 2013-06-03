67700fd25cb472cf7476bc46736330c3828aed48
Stefan Niederhauser <stefan.niederhauser@mimacom.com>
2013-06-03 22:05:59 +0200

    added 'run' and 'startServer' goals.

---
+++ src/main/java/com/kelveden/karma/AbstractStartMojo.java
@@ -0,0 +1,156 @@
+/**
+ * Copyright 2013 Alistair Dutton
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.kelveden.karma;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.codehaus.plexus.util.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ */
+public abstract class AbstractStartMojo extends AbstractKarmaMojo {
+
+    /**
+     * Path to the Karma configuration file.
+     */
+    @Parameter(defaultValue = "${basedir}/karma.conf.js", property = "configFile", required = true)
+    protected File configFile;
+
+    /**
+     * Comma-separated list of browsers. See the "browsers" section of the Karma online configuration documentation for
+     * supported values.
+     */
+    @Parameter(property = "browsers", required = false)
+    protected String browsers;
+
+    /**
+     * Flag indicating whether the Karma server will automatically re-run when watched files change. See the "autoWatch"
+     * section of the Karma online configuration documentation for more information. Defaults to Karma default.
+     */
+    @Parameter(property = "autoWatch", required = false)
+    protected Boolean autoWatch;
+
+    /**
+     * Comma-separated list of reporters. See the "reporters" section of the Karma online configuration documentation for
+     * supported values.
+     */
+    @Parameter(property = "reporters", required = false)
+    protected String reporters;
+
+    /**
+     * Browser capture timeout in milliseconds. See the "captureTimeout" section of the Karma online configuration documentation for
+     * more information.
+     */
+    @Parameter(property = "captureTimeout", required = false)
+    protected Integer captureTimeout;
+
+    /**
+     * Threshold (in milliseconds) beyond which slow-running tests will be reported. See the "reportSlowerThan" section
+     * of the Karma online configuration documentation for more information.
+     */
+    @Parameter(property = "reportSlowerThan", required = false)
+    protected Integer reportSlowerThan;
+
+    protected Process startKarmaProcess(ProcessBuilder builder) throws MojoExecutionException {
+        try {
+
+            System.out.println(StringUtils.join(builder.command().iterator(), " "));
+
+            return builder.start();
+
+        } catch (IOException e) {
+            throw new MojoExecutionException("There was an error executing Karma.", e);
+        }
+    }
+
+    protected void addKarmaArguments(List<String> command) {
+        command.addAll(valueToKarmaArgument(browsers, "--browsers"));
+        command.addAll(valueToKarmaArgument(reporters, "--reporters"));
+        command.addAll(valueToKarmaArgument(autoWatch, "--auto-watch", "--no-auto-watch"));
+        command.addAll(valueToKarmaArgument(captureTimeout, "--capture-timeout"));
+        command.addAll(valueToKarmaArgument(reportSlowerThan, "--report-slower-than"));
+    }
+
+    protected boolean executeKarma(final Process karma) throws MojoExecutionException {
+
+        BufferedReader karmaOutputReader = null;
+        try {
+            karmaOutputReader = createKarmaOutputReader(karma);
+
+            for (String line = karmaOutputReader.readLine(); line != null; line = karmaOutputReader.readLine()) {
+                System.out.println(line);
+            }
+
+            return (karma.waitFor() == 0);
+
+        } catch (IOException e) {
+            throw new MojoExecutionException("There was an error reading the output from Karma.", e);
+
+        } catch (InterruptedException e) {
+            throw new MojoExecutionException("The Karma process was interrupted.", e);
+
+        } finally {
+            IOUtils.closeQuietly(karmaOutputReader);
+        }
+    }
+
+    protected List<String> valueToKarmaArgument(final Boolean value, final String trueSwitch, final String falseSwitch) {
+        if (value == null) {
+            return Collections.EMPTY_LIST;
+        }
+
+        if (value.booleanValue()) {
+            return Arrays.asList(trueSwitch);
+        } else {
+            return Arrays.asList(falseSwitch);
+        }
+    }
+
+    protected List<String> valueToKarmaArgument(final Integer value, final String argName) {
+        if (value == null) {
+            return Collections.EMPTY_LIST;
+        }
+
+        return Arrays.asList(argName, String.valueOf(value));
+    }
+
+    protected List<String> valueToKarmaArgument(final String value, final String argName) {
+        if (value == null) {
+            return Collections.EMPTY_LIST;
+        }
+
+        return Arrays.asList(argName, value);
+    }
+
+    protected BufferedReader createKarmaOutputReader(final Process p) {
+        return new BufferedReader(new InputStreamReader(p.getInputStream()));
+    }
+
+    protected boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().contains("windows");
+    }
+}
--- src/main/java/com/kelveden/karma/RunMojo.java
+++ src/main/java/com/kelveden/karma/RunMojo.java
@@ -18,30 +18,26 @@ package com.kelveden.karma;
 import org.apache.commons.io.IOUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.codehaus.plexus.util.StringUtils;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 /**
- * Executes the 'start' task against Karma. See the Karma documentation itself for information: http://karma.github.com.
+ * Executes the 'run' task against Karma. Should be used together with the 'startServer' goal.
+ * See the Karma documentation itself for information: http://karma.github.com.
  */
-@Mojo(name = "start", defaultPhase = LifecyclePhase.TEST)
-public class StartMojo extends AbstractStartMojo {
-
-    /**
-     * Flag indicating whether the Karma server will exit after a single test run. See the "singleRun" section of
-     * the Karma online configuration documentation for more information. Defaults to true.
-     */
-    @Parameter(property = "singleRun", required = false, defaultValue = "true")
-    private Boolean singleRun;
+@Mojo(name = "run", defaultPhase = LifecyclePhase.TEST)
+public class RunMojo extends AbstractKarmaMojo {
 
     public void execute() throws MojoExecutionException, MojoFailureException {
 
@@ -52,7 +48,7 @@ public class StartMojo extends AbstractStartMojo {
 
         final Process karma = createKarmaProcess();
 
-        if (!executeKarma(karma) && singleRun) {
+        if (!executeKarma(karma)) {
             if (karmaFailureIgnore) {
                 getLog().warn("There were Karma test failures.");
             } else {
@@ -63,25 +59,60 @@ public class StartMojo extends AbstractStartMojo {
         System.out.flush();
     }
 
+    private boolean executeKarma(final Process karma) throws MojoExecutionException {
+
+        BufferedReader karmaOutputReader = null;
+        try {
+            karmaOutputReader = createKarmaOutputReader(karma);
+
+            for (String line = karmaOutputReader.readLine(); line != null; line = karmaOutputReader.readLine()) {
+                System.out.println(line);
+            }
+
+            return (karma.waitFor() == 0);
+
+        } catch (IOException e) {
+            throw new MojoExecutionException("There was an error reading the output from Karma.", e);
+
+        } catch (InterruptedException e) {
+            throw new MojoExecutionException("The Karma process was interrupted.", e);
+
+        } finally {
+            IOUtils.closeQuietly(karmaOutputReader);
+        }
+    }
+
     private Process createKarmaProcess() throws MojoExecutionException {
 
         final ProcessBuilder builder;
 
         if (isWindows()) {
-            builder = new ProcessBuilder("cmd", "/C", "karma", "start", configFile.getAbsolutePath());
+            builder = new ProcessBuilder("cmd", "/C", "karma", "run");
         } else {
-            builder = new ProcessBuilder("karma", "start", configFile.getAbsolutePath());
+            builder = new ProcessBuilder("karma", "run");
         }
 
         final List<String> command = builder.command();
 
-        command.addAll(valueToKarmaArgument(singleRun, "--single-run", "--no-single-run"));
-        addKarmaArguments(command);
-
         builder.redirectErrorStream(true);
 
-        return startKarmaProcess(builder);
+        try {
+
+            System.out.println(StringUtils.join(command.iterator(), " "));
+
+            return builder.start();
+
+        } catch (IOException e) {
+            throw new MojoExecutionException("There was an error executing Karma.", e);
+        }
     }
 
+    private BufferedReader createKarmaOutputReader(final Process p) {
+        return new BufferedReader(new InputStreamReader(p.getInputStream()));
+    }
+
+    private boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().contains("windows");
+    }
 
 }
